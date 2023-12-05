d6065f2c9316af516d83d5473368b9c020b04d64
jitendra <gangwarjitendra2498@gmail.com>
2023-12-05 23:56:57 +0530

    feat:header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,20 @@ const Header = () => {
  return (
    <header className="bg-green-500">
      <div className="container mx-auto flex items-center justify-between p-4">
-       <h1 className="rounded-xl py-1 px-3 text-4xl font-bold text-complementary shadow-neutral [transition:color_.5s_ease-in-out,box-shadow_.5s_ease-in-out] hover:text-highlight-300 hover:shadow-hover">
-         <Link href="/">AgriMartX</Link>
+       <h1 className="rounded-xl py-1 px-3 text-4xl font-bold text-complementary shadow-neutral [transition:color_.5s_ease-in-out,box-shadow_.5s_ease-in-out] hover:text-highlight-600 hover:bg-green-600">
+         <Link href="/">AgriMart</Link>
        </h1>
        <nav className="flex items-center gap-10">
          <ul className="flex gap-6 text-2xl font-semibold">
-           <li className="mr-6 text-complementary ">
+           <li className="mr-6 text-complementary">
              <Link href="/">
                <h3 className="relative inline-block transition-all after:absolute after:bottom-0 after:left-1/2 after:block after:h-0.5 after:w-0 after:bg-neutral after:transition-all after:content-[''] hover:text-neutral hover:after:left-0 hover:after:w-full">
-                 Consumer
+                 Home
                </h3>
              </Link>
            </li>
            <li className=" mr-6 text-complementary ">
-             <Link href="/">
+             <Link href="/producer">
                <h3 className="relative inline-block text-complementary transition-all after:absolute after:bottom-0 after:left-1/2 after:block after:h-0.5 after:w-0 after:bg-neutral after:transition-all after:content-[''] hover:text-neutral hover:after:left-0 hover:after:w-full">
                  Producer
                </h3>
@@ -28,15 +28,15 @@ const Header = () => {
            <li className=" mr-6 text-complementary ">
              <Link href="/">
                <h3 className="relative inline-block text-complementary transition-all after:absolute after:bottom-0 after:left-1/2 after:block after:h-0.5 after:w-0 after:bg-neutral after:transition-all after:content-[''] hover:text-neutral hover:after:left-0 hover:after:w-full">
-                 
+                 Consumer
                </h3>
              </Link>
            </li>
          </ul>
          <ConnectButton
-            showBalance={false}
-            //  chainStatus="none"
-           ></ConnectButton>
+           //showBalance={false}
+           chainStatus="none"
+         ></ConnectButton>
        </nav>
      </div>
    </header>
@@ -45,8 +45,3 @@ const Header = () => {
 
 
 export default Header;
-
-
-
-
-
