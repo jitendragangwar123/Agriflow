b272e9f3de72ca68b279b97296fc38f5d5e3000c
jitendra <gangwarjitendra2498@gmail.com>
2023-12-08 13:35:23 +0530

    feat:consume registration page added

---
+++ src/pages/consumer.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import { ToastProvider } from "react-toast-notifications";
+import RegisterNewConsumer from "../components/consumer/RegisterNewConsumer";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+
+const RegistrationPage: NextPage = () => {
+ return (
+   <>
+     <Head>
+       <title>Consumer Registration</title>
+       <meta name="description" content="Producer Registration" />
+       <link rel="icon" href="/favicon.ico" />
+     </Head>
+     <Header />
+     <ToastProvider>
+       <RegisterNewConsumer />
+     </ToastProvider>
+     <Footer />
+   </>
+ );
+};
+
+
+export default RegistrationPage;
