0f675308a060430ad9b5682a84133565cd624f0c
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-01-06 14:49:10 +0530

    feat:wallet theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,49 +1,58 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { polygonMumbai, polygon, mainnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
-
 const { chains, publicClient, webSocketPublicClient } = configureChains(
- [
-   polygonMumbai,
-   polygon,
-   mainnet,
-   ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-     ? [polygonMumbai]
-     : []),
- ],
- [publicProvider()]
+  [
+    polygonMumbai,
+    polygon,
+    mainnet,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+      ? [polygonMumbai]
+      : []),
+  ],
+  [publicProvider()]
 );
 
-
 const { connectors } = getDefaultWallets({
- appName: "RainbowKit App",
- projectId: "YOUR_PROJECT_ID",
- chains,
+  appName: "RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
 });
-
 
 const wagmiConfig = createConfig({
- autoConnect: true,
- connectors,
- publicClient,
- webSocketPublicClient,
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
 });
 
-
 function MyApp({ Component, pageProps }: AppProps) {
- return (
-   <WagmiConfig config={wagmiConfig}>
-     <RainbowKitProvider chains={chains}>
-       <Component {...pageProps} />
-     </RainbowKitProvider>
-   </WagmiConfig>
- );
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        modalSize="compact"
+        theme={darkTheme({
+          accentColor: "#7b3fe8",
+          accentColorForeground: "white",
+          borderRadius: "medium",
+          fontStack: "system",
+          overlayBlur: "small",
+        })}
+        chains={chains}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
-
 
 export default MyApp;
