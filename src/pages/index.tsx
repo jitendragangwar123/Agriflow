41876a3d5acb35d36d7ba282efa36322a4bf9f0a
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-01-23 23:22:31 +0530

    refactor:index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,18 @@ import Head from "next/head";
 import Layout from "../layouts/Layout";
 
 const Home: NextPage = () => {
- return (
-   <>
-     <Head>
-       <title>AgriFlow</title>
-       <meta name="description" content="AgriFlow" />
-       <link rel="icon" href="/favicon.ico" />
-     </Head>
-    
-     <Layout>
-       <div>
-       </div>
-     </Layout>
-   </>
- );
+  return (
+    <>
+      <Head>
+        <title>AgriFlow</title>
+        <meta name="description" content="AgriFlow" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div></div>
+      </Layout>
+    </>
+  );
 };
 
 export default Home;
